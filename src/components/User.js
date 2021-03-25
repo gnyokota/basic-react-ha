c1a020eb85fb006226021523f6b8910d0b17f2ec
Giovana Yokota <gnyokota@gmail.com>
2021-03-25 14:07:59 +0100

    material-ui styles added

--- src/components/User.js
+++ src/components/User.js
@@ -1,23 +1,54 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
-import {Grid, makeStyles, Avatar} from '@material-ui/core';
+import {Grid, makeStyles, Avatar, Typography, Box, Button} from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 const useStyles = makeStyles(theme => (
-    {
-        root:{
-            border:`1.2px solid #e0e0e0`,
-            padding: theme.spacing(2),
+    {   background:{
+        backgroundColor: '#fddb3a',
+        width: '100vw',
+        minHeight: '100vh',
+    },
+        title:{
+            color:'#41444b',
+            padding:'1rem',
+            fontWeight:900,
+        },
+        card:{
+            padding: '2rem 0.5rem',
             borderRadius: '8px',
-            maxWidth: 250, 
+            maxWidth: 220, 
+            margin:'1rem',
+            backgroundColor:'white'
         },
         avatar:{
-            width: theme.spacing(7),
-            height: theme.spacing(7), 
-            color: '#e7d9ea',
-            backgroundColor: '#16c79a',
+            width: '60px',
+            height: '60px', 
+            color: '#52575d',
+            backgroundColor: '#fddb3a',
+            marginBottom:'1rem'
         }, 
-        name:{
-            fontWeight: theme.typography.fontWeightBold
+        name: {
+            align: 'center',
+            color:'#41444b',
+            marginBottom:'1rem'
+           
+        }, 
+        username:{
+            color: '#a6a9b6',
+            marginBottom:'0.5rem'
+        }, 
+        web:{
+            color: '#31326f'
+        }, 
+        link:{
+            textDecoration:'none',
+        },
+        button:{
+            color:'#41444b',
+            backgroundColor:"#fddb3a",
+            fontWeight:600,
+            margin:'0.5rem'
         }
     }
 ))
@@ -41,17 +72,45 @@ function User() {
     const classes = useStyles(); 
 
     return (
-        <Grid container justify='center' spacing={4}>
+        <div className={`${classes.background} --user-section`}>
+            {/* title */}
+        <Typography variant='h3' align='center' className={classes.title}>
+            LIST OF USERS
+        </Typography>
+            {/* wrapper */}
+        <Grid container spacing={0} alignContent='center'justify="center" className={classes.wrapper} >
             {data.map(user=> (
-                <Grid container direction='column' alignItems='center' className={classes.root}>
-                    <Avatar className={classes.avatar}>{user.name[0]}</Avatar>
-                    <h1>{user.name}</h1>
-                    <p>@{user.username}</p>
-                    <a href={`http://${user.website}`}>{`http://${user.website}`}</a>
-                    <Link to={`/user/${user.id}`}><button>More Details</button></Link>
-                </Grid>
+                // Card 
+                <Box key={user.id}
+                boxShadow={2} component={Grid} container direction='column' justifyContent='space-between' alignItems='center'  className={classes.card}>
+                    {/* avatar with the initial */}
+                    <Avatar className={classes.avatar}>
+                        {user.name[0]}
+                    </Avatar>
+
+                    {/* name */}
+                    <Typography variant='h5' align='center' className={classes.name}>
+                        {user.name}
+                    </Typography>
+
+                    {/* username */}
+                    <Typography variant='caption' className={classes.username}>
+                        @{user.username}
+                    </Typography>
+
+                    {/* Website link  */}
+                    <Typography variant='caption'>
+                    <a href={`http://${user.website}`} className={classes.web}>{`http://${user.website}`}</a>
+                    </Typography>
+
+                    {/* link and button  */}
+                    <Link to={`/user/${user.id}`} className={classes.link}>
+                    <Button variant="contained" className={classes.button} endIcon={<NavigateNextIcon/>}>More Detail</Button>
+                    </Link>
+                </Box>
             ))}
         </Grid>
+        </div>
     )
 }
 
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,9 +1,58 @@
 import React, {useState,useEffect} from 'react'; 
 import {useParams, Link} from 'react-router-dom';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import {makeStyles, Box, Grid, Typography, List, ListItem, ListItemText, Button} from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
+const useStyles = makeStyles(theme => (
+    {   
+        colorPrimary: {
+            backgroundColor: '#41444b',
+          },
+          barColorPrimary: {
+            backgroundColor: "#fddb3a",
+          },
+        box:{
+            maxWidth:'400px',
+            borderRadius:'8px',
+            background:"#fddb3a",
+            margin: '4vh auto', 
+            padding: '1rem 2rem'
+        },
+        list:{
+            margin: '1rem auto'
+        },
+        listItem:{
+            paddingBottom:0, 
+            paddingTop:0,
+            align:'center'
+        },
+        primaryColor:{
+            color:'#41444b'
+        },
+        secondColor:{
+            color:'#3a6351'
+        },
+        link:{
+            textDecoration:'none',
+        }, 
+        button:{
+            color:"#fddb3a",
+            backgroundColor:'#41444b',
+            fontWeight:600,
+            margin:'0.5rem auto',
+            '&:hover': {
+                color: '#41444b'
+            }
+        }
+        
+    }
+))
 
 function UserDetails() {
-    const [user,setUser]= useState([]);
+    //to avoid the problem with the initial true value using []
+    //and the nested objects, a should use '' or false 
+    const [user,setUser]= useState('');
 
     //use params from the url path :id
     const { id }=useParams(); 
@@ -11,6 +60,7 @@ function UserDetails() {
     const url = `https://jsonplaceholder.typicode.com/users/${id}`;
    
     useEffect(()=>{
+        
         const getIndividualData = async () => {
             try{
                 const data = await(await fetch(url)).json(); 
@@ -19,39 +69,63 @@ function UserDetails() {
                 console.error(error.message);
             }
         }
-        getIndividualData();
+        // setTimeout(function(){
+                getIndividualData();
+        // },3000)
     },[url])
 
+    const classes = useStyles(); 
+
     return (
-        <div>
-            {user && 
-               (<div>
-                <p>Name: {user.name}</p>
-                <p>Username: {user.username}</p>
-                <p>E-mail: {user.email}</p>
-                <p>Phone: {user.phone}</p>
-                <p>Website: <a href={`http://${user.website}`}>{user.website}</a></p>
-                </div>)
-            }{
-                user.company&& 
-                (
-                <p>Company: {user.company.name}</p>
-                )
-            }{
-                user.address &&
-                (
-                <div>
-                <p>Address:</p>
-                    <ul>
-                        <li>Street: {user.address.street}</li>
-                        <li>Suite: {user.address.suite}</li>
-                        <li>City: {user.address.city}</li>
-                        <li>Zipcode: {user.address.zipcode}</li>
-                    </ul>
-                </div>
-                )
-            }
-            <Link to='/' ><button>BACK</button></Link>
+        <div className='user-section'>
+            {user? (
+                //This is my yellow box
+                    <Box  component={Grid} container boxShadow={2} justifyContent="center" alignItems="center" className={classes.box}>
+                        {/* this grid area constains the list with the user data: */}
+                        <Grid item lg={6} sm={12}>
+                            <List className={classes.list}>
+                                <ListItem alignItems="flex-start"  className={classes.listItem}>
+                                   <ListItemText primary="Name" secondary={<Typography variant='body2'className={classes.secondColor} >{user.name}</Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.secondColor.listItem}>
+                                   <ListItemText primary="Username" secondary={<Typography variant='body2'className={classes.secondColor}>{user.username}</Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.listItem}>
+                                   <ListItemText primary="Email" secondary={<Typography variant='body2'className={classes.secondColor}>{user.email}</Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.listItem}>
+                                   <ListItemText primary="Phone" secondary={<Typography variant='body2'className={classes.secondColor}>{user.phone}</Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.listItem}>
+                                   <ListItemText primary="Company" secondary={<Typography variant='body2'className={classes.secondColor}>{user.company.name}</Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.listItem}>
+                                   <ListItemText primary="Website" secondary={<Typography variant='body2'className={classes.secondColor}><a className={classes.secondColor.fontColor} href={`http://${user.website}`}>http://{user.website}</a></Typography>} />
+                                </ListItem>
+                                <ListItem alignItems="flex-start" className={classes.listItem}>
+                                <ListItemText primary="Address" 
+                                    secondary={<Typography variant='body2'className={classes.secondColor}>
+                                               Street: {user.address.street}<br/>
+                                               Suite: {user.address.suite}<br/>
+                                               City: {user.address.city}<br/>
+                                               Zipcode: {user.address.zipcode}<br/>
+                                               </Typography>
+                                    } 
+                                    />  
+                                </ListItem>
+                            </List>
+                        </Grid>
+                        {/* this grid area constains the button: */}
+                        <Grid item lg={6} sm={12}>
+                            <Link to='/'className={classes.link} >
+                                <Button variant="contained" className={classes.button} startIcon={<ArrowBackIosIcon/>}>BACK TO LIST</Button>
+                            </Link>
+                        </Grid>
+                    </Box>
+                ): 
+                    <Box m='3rem auto'>
+                    <LinearProgress classes={{colorPrimary: classes.colorPrimary, barColorPrimary: classes.barColorPrimary}}/>
+                    </Box>}
         </div>
     )
 }
